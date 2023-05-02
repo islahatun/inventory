52de8340e771dbc65fcabf637ac31e8be7d4af40
islahatun <islahatunnufusi98@gmail.com>
2023-05-03 00:19:56 +0700

    tempates

---
+++ application/views/_parent/overview.php
@@ -0,0 +1,23 @@
+<!-- ======= HEADER =======  -->
+<?php $this->load->view('_parent/header') ?>
+<!-- ======= END HEADER =======  -->
+
+<!-- ======= SIDEBAR ======= -->
+<?php $this->load->view('_parent/sidebar') ?>
+<!--  ======= END SIDEBAR ======= -->
+
+
+<!-- ======= MAIN ======= -->
+
+<main id="main" class="main">
+
+    <?= $content_overview ?>
+
+</main>
+
+<!--  ======= END MAIN ======= -->
+
+
+<!-- ======= FOOTER ======= -->
+<?php $this->load->view('_parent/footer') ?>
+<!--  ======= END FOOTER ======= -->
