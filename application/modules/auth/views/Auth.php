77f7877d1104364c68b353f9004a8bd6d618534a
islahatun <islahatunnufusi98@gmail.com>
2023-06-15 13:07:13 +0700

    perbaikan nama aplikasi

--- application/modules/auth/views/Auth.php
+++ application/modules/auth/views/Auth.php
@@ -63,7 +63,7 @@
                             <div class="d-flex justify-content-center py-4">
                                 <a href="index.html" class="logo d-flex align-items-center w-auto">
                                     <img src="assets/img/logo.png" alt="">
-                                    <span class="d-none d-lg-block">INVENTORY</span>
+                                    <span class="d-none d-lg-block">PT. SURYAMAS MENTARI</span>
                                 </a>
                             </div><!-- End Logo -->
 
--- application/views/_parent/header.php
+++ application/views/_parent/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Components / Accordion - NiceAdmin Bootstrap Template</title>
+    <title>PT. SURYAMAS MENTARI</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
@@ -73,7 +73,7 @@
         <div class="d-flex align-items-center justify-content-between">
             <a href="index.html" class="logo d-flex align-items-center">
                 <img src="<?= base_url('assets/') ?>assets/img/logo.png" alt="">
-                <span class="d-none d-lg-block">INVENTORY</span>
+                <span class="d-none d-lg-block text-center">PT. SURYAMAS MENTARI</span>
             </a>
             <i class="bi bi-list toggle-sidebar-btn"></i>
         </div><!-- End Logo -->
